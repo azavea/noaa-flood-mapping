d74f8b69bc2196d3f68cb766f1e034e9e4256e28
CloudNiner <andy@finkmiller.com>
2020-07-30 13:59:16 -0400

    Parameterize on experiment and test+train split args

--- catalogs/sen1floods11-mldata/main.py
+++ catalogs/sen1floods11-mldata/main.py
@@ -1,3 +1,4 @@
+import argparse
 from copy import deepcopy
 
 from pystac import (
@@ -10,7 +11,21 @@ from pystac import (
 from sklearn.model_selection import train_test_split
 
 
-EXPERIMENT = {"s2weak": {"labels": "NoQC"}}
+EXPERIMENT = {"s2weak": "NoQC", "s1weak": "S1Flood_NoQC", "hand": "QC_v2"}
+
+
+def check_experiment(val):
+    if val in set(EXPERIMENT.keys()):
+        return val
+    else:
+        raise ValueError
+
+
+def normalized_float(val):
+    float_val = float(val)
+    if float_val < 0 or float_val > 1:
+        raise ValueError
+    return float_val
 
 
 def mapper(item):
@@ -39,10 +54,53 @@ def mapper(item):
 
 
 def main():
+    """
+
+    TODOS:
+    - [ ] Add self links to sen1floods11 stac by pulling, loading and rewriting the catalog from s3
+    - [ ] Publish updated sen1floods11 catalog
+    - [ ] Test performance of this code reading from the remote catalog
+    - [ ] Update this code to add link to source item in sen1floods11 stack, requires (1)
+    - [ ] Update this code to the schema discussed in https://github.com/azavea/noaa-flood-mapping/issues/38#issuecomment-667178703
+    - [ ] Do we need to parameterize this code with absolute urls to s3 bucket?
+
+    """
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "experiment",
+        type=check_experiment,
+        help="Experiment to generate. One of {}".format(set(EXPERIMENT.keys())),
+    )
+    parser.add_argument(
+        "--test-size",
+        "-t",
+        default=0.3,
+        dest="test_size",
+        type=normalized_float,
+        help="Percentage of dataset to use for test set",
+    )
+    parser.add_argument(
+        "--train-size",
+        "-T",
+        default=None,
+        dest="train_size",
+        type=normalized_float,
+        help="Percentage of dataset to use for train set",
+    )
+    parser.add_argument(
+        "--random-seed",
+        "-r",
+        dest="random_seed",
+        default=None,
+        type=int,
+        help="Random seed for generating test / train split. Passing the same value will yield the same splits.",
+    )
+    args = parser.parse_args()
+
     catalog = Catalog.from_file("./data/catalog/catalog.json")
 
-    experiment = "s2weak"
-    label_collection_id = EXPERIMENT[experiment]["labels"]
+    experiment = args.experiment
+    label_collection_id = EXPERIMENT[experiment]
     label_collection = catalog.get_child(label_collection_id)
 
     mldata_catalog = Catalog(
@@ -63,7 +121,10 @@ def main():
     print("Constructing new label items...")
     label_items = map(mapper, label_collection.get_items())
     train_items, test_items = train_test_split(
-        list(label_items), test_size=0.3, random_state=42
+        list(label_items),
+        test_size=args.test_size,
+        train_size=args.train_size,
+        random_state=args.random_seed,
     )
 
     train_collection.add_items(train_items)
