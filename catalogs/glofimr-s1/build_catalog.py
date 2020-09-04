6fb16145157fa871df89e9f160cae79a4ee69239
CloudNiner <andy@finkmiller.com>
2020-09-04 12:59:47 -0400

    WIP: Update ipynb and build_catalog with coregister

--- catalogs/glofimr-s1/build_catalog.py
+++ catalogs/glofimr-s1/build_catalog.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 import geopandas as gpd
 import pystac
 
+from coregister import coregister_raster
 from stac_df import pystac_catalog_to_dataframe
 
 
@@ -13,8 +14,12 @@ def https_to_s3_url(https_url_str):
 
 
 def assets_to_s3_url(row):
-    row['assets_hand'] = https_to_s3_url(row.get('assets_hand', {}).get('hand', {}).get('href', ''))
-    row['assets_sar'] = https_to_s3_url(row.get('assets_sar', {}).get('sar', {}).get('href', ''))
+    row["assets_hand"] = https_to_s3_url(
+        row.get("assets_hand", {}).get("hand", {}).get("href", "")
+    )
+    row["assets_sar"] = https_to_s3_url(
+        row.get("assets_sar", {}).get("sar", {}).get("href", "")
+    )
     return row
 
 
@@ -30,9 +35,20 @@ def main():
     hand_df = pystac_catalog_to_dataframe(hand_catalog)
     hand_df = hand_df.to_crs("EPSG:4326")
 
-    sar_hand_df = gpd.sjoin(sar_df, hand_df, op="intersects", how="inner", lsuffix="sar", rsuffix="hand")
-    sar_hand_df = sar_hand_df[['geometry', 'bbox_sar', 'index_hand', 'assets_hand', 'assets_sar']]
+    sar_hand_df = gpd.sjoin(
+        sar_df, hand_df, op="intersects", how="inner", lsuffix="sar", rsuffix="hand"
+    )
+    sar_hand_df = sar_hand_df[
+        ["geometry", "bbox_sar", "index_hand", "assets_hand", "assets_sar"]
+    ]
     sar_hand_df = sar_hand_df.apply(assets_to_s3_url, axis=1)
+
+    coregister_raster(
+        "s3://hand-data/080102/080102hand.tif",
+        "s3://glofimr-sar-4326/1/654e5272-a61c-4d69-bc71-aef74ccaf85f/16SCF_0_4/MASK.tiff",
+        "./data/16SCF_0_4-HAND.tiff",
+    )
+
 
 if __name__ == "__main__":
     main()
