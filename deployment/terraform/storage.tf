7a47c4385fd2dbc5187d20cbdcb7df2164ac85e0
Cole Kettler <me@colekettler.com>
2020-10-02 17:08:12 -0400

    Create ECR repositories for catalogs and pipeline images

--- deployment/terraform/storage.tf
+++ deployment/terraform/storage.tf
@@ -1,3 +1,17 @@
+module "ecr_catalogs" {
+  source = "github.com/azavea/terraform-aws-ecr-repository?ref=1.0.0"
+
+  repository_name         = "noaa-flood-catalogs"
+  attach_lifecycle_policy = true
+}
+
+module "ecr_pipeline" {
+  source = "github.com/azavea/terraform-aws-ecr-repository?ref=1.0.0"
+
+  repository_name         = "noaa-flood-pipeline"
+  attach_lifecycle_policy = true
+}
+
 resource "aws_s3_bucket" "data" {
   bucket = "noaafloodmap-data-${var.aws_region}"
   acl    = "private"
